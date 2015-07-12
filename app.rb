5159702b314f583a34e11b8ba9c054148c06fc2f
Tejas Manohar <me@tejas.io>
2015-07-12 03:17:03 -0400

    completed directions

--- app.rb
+++ app.rb
@@ -5,16 +5,19 @@ require 'sinatra'
 require 'google_directions'
 require 'twilio-ruby'
 require 'nokogiri'
+require 'sanitize'
 
 @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
 get '/' do
   response = GoogleDirections.new("130 brown street waltham", "moes waltham ma")
   doc = Nokogiri::XML(response.xml)
+  directions = ''
   doc.xpath('/DirectionsResponse/route/leg/step/html_instructions').each do |step|
-    p step.content
+    step = Sanitize.fragment("#{step.content}\n")
+    directions << step
   end
-  'hello'
+  directions
 end
 
 post '/sms' do
