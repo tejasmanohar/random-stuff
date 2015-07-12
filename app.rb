39c9b6728e412c9885f59fd12d42a1fce5ccd19a
Tejas Manohar <me@tejas.io>
2015-07-12 02:54:00 -0400

    gdirections

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'dotenv'
 Dotenv.load
 
 require 'sinatra'
+require 'google_directions'
 require 'twilio-ruby'
 
 @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
