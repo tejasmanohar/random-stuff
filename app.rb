1b0d507dd1b396ebae186302f801c4bcf98db072
Tejas Manohar <me@tejas.io>
2015-07-12 03:01:07 -0400

    nokogiri

--- app.rb
+++ app.rb
@@ -4,11 +4,14 @@ Dotenv.load
 require 'sinatra'
 require 'google_directions'
 require 'twilio-ruby'
+require 'nokogiri'
 
 @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
 get '/' do
-  'hello world'
+  directions = GoogleDirections.new("130 brown street waltham", "moes waltham ma")
+  content_type 'text/xml'
+  directions.xml
 end
 
 post '/sms' do
